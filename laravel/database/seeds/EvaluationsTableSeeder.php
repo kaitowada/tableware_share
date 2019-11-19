d1440e8094be7a49d8de7026c93b94a10f795bc6
和田海斗 <kaito@diffeasy.com>
2019-11-19 12:45:38 +0900

    feat : Evaluation@show作成

--- laravel/database/seeds/EvaluationsTableSeeder.php
+++ laravel/database/seeds/EvaluationsTableSeeder.php
@@ -13,8 +13,16 @@ class EvaluationsTableSeeder extends Seeder
     {
         for($i = 0; $i < 1000; $i++) {
             DB::table('evaluations')->insert([
-                'user_id' => mt_rand(1, 10),
+                'user_id' => mt_rand(1, 9),
                 'star' => mt_rand(1, 5),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ]);
+        }
+        for($i = 0; $i < 10; $i++) {
+            DB::table('evaluations')->insert([
+                'user_id' => 10,
+                'star' => 1,
                 'created_at' => date("Y-m-d H:i:s"),
                 'updated_at' => date("Y-m-d H:i:s")
             ]);
