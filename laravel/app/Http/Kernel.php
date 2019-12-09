dbe67b2934dcdc74247530c7d9e80db3a7baab4f
和田海斗 <kaito@diffeasy.com>
2019-12-09 09:28:46 +0900

    feat : corsのプラグインがlaravel6に対応していたのでプラグインを入れた

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Barryvdh\Cors\HandleCors::class,
             'throttle:60,1',
             'bindings',
         ],
@@ -61,7 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \App\Http\Middleware\Cors::class,
+        // 'cors' => \App\Http\Middleware\Cors::class,
     ];
 
     /**
